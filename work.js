d1b9f0bf76a2d52fad400c99c9727156ebfac168
Sanjid Talukder <sanjidtalukder02@gmail.com>
2024-11-01 17:02:19 +0600

    Register form

--- work.js
+++ work.js
@@ -1,15 +1,52 @@
-const slideshowElements = document.querySelectorAll(".slideshow-element");
+let passInput=document.getElementById("password");
+let letter=document.getElementById("letter");\
+let capital=document.getElementById("capital");
+let number=document.getElementById("number");
+let length=document.getElementById("length");
 
-let countElements = 0;
-setInterval(() => {
-    let currentElement = document.querySelector(".current");
-    currentElement.classList.remove("current");
+passInput.onfocus=function(){
+    document.getElementById("message").style.display="block";
+}
 
-    countElements++;
-    if (countElements >= slideshowElements.length) {
-        slideshowElements[0].classList.add("current");
-        countElements = 0;
-    } else {
-        currentElement.nextElementSibling.classList.add("current");
+passInput.onblur=function(){
+    document.getElementById("message").style.display="none";
+}
+
+passInput.onkeyup=function(){
+    let lowerCaseletters=/[a-z]/g;
+
+    if(passInput.Value.match(lowerCaseletters)){
+        letter.classList.remove("invalid");
+        letter.classList.add("valid");
+    }else{
+        letter.classList.remove("valid");
+        letter.classList.add("invalid");
     }
-}, 2000);
+    let upperCaseLetters=/[A-z]/g;
+
+    if(passInput.Value.match(upperCaseletters)){
+        capital.classList.remove("invalid");
+        capital.classList.add("valid");
+    }else{
+        capital.classList.remove("valid");
+        capital.classList.add("invalid");
+    }
+
+    let numbers=/[0-9]/g;
+    if(passInput.Value.match(numbers)){
+        number.classList.remove("invalid");
+        number.classList.add("valid");
+    }else{
+        number.classList.remove("valid");
+        number.classList.add("invalid");
+    }
+
+    if(passInput.Value.length>=8){
+        length.classList.remove("invalid");
+        length.classList.add("valid");
+    }else{
+        length.classList.remove("valid");
+        length.classList.add("invalid");
+    }
+
+}
