20a9fb741a2db48489f91ea1e6ab9aded22bdda3
Sanjid Talukder <sanjidtalukder02@gmail.com>
2024-11-01 16:59:46 +0600

    Mini project

--- work.js
+++ work.js
@@ -1,50 +1,15 @@
+const slideshowElements = document.querySelectorAll(".slideshow-element");
+
+let countElements = 0;
 setInterval(() => {
-    // get time indicator elements
-    let hours = document.getElementById('hours');
-    let minutes = document.getElementById('minutes');
-    let secondes = document.getElementById('seconds');
-    let ampm = document.getElementById('ampm');
-  
-    // digits time indicator
-    let hh = document.getElementById('hh');
-    let mm = document.getElementById('mm');
-    let ss = document.getElementById('ss');
-  
-  
-    // dot time indicator
-    let dotH = document.querySelector('.h_dot');
-    let dotM = document.querySelector('.m_dot');
-    let dotS = document.querySelector('.s_dot');
-  
-    // get current time
-    let h = new Date().getHours();
-    let m = new Date().getMinutes();
-    let s = new Date().getSeconds();
-    let ap = h >= 12 ? 'PM' : 'AM';
-  
-    // convert to 12 hour format
-    if (h > 12) {
-      h = h - 12;
+    let currentElement = document.querySelector(".current");
+    currentElement.classList.remove("current");
+
+    countElements++;
+    if (countElements >= slideshowElements.length) {
+        slideshowElements[0].classList.add("current");
+        countElements = 0;
+    } else {
+        currentElement.nextElementSibling.classList.add("current");
     }
-  
-    // add 0 before single digit
-    h = h < 10 ? '0' + h : h;
-    m = m < 10 ? '0' + m : m;
-    s = s < 10 ? '0' + s : s;
-  
-    // set time and label
-    hours.innerHTML = h + 'Hours';
-    minutes.innerHTML = m + 'Minutes';
-    secondes.innerHTML = s + 'Seconds';
-    ampm.innerHTML = ap;
-  
-    // set time circular indicator
-    hh.style.strokeDashoffset = 440 - (440 * h) / 12;
-    mm.style.strokeDashoffset = 440 - (440 * m) / 60;
-    ss.style.strokeDashoffset = 440 - (440 * s) / 60;
-  
-    // set dot time position indicator
-    dotH.style.transform = `rotate(${h * 30}deg)`;
-    dotM.style.transform = `rotate(${m * 6}deg)`;
-    dotS.style.transform = `rotate(${s * 6}deg)`;
-  }, 1000);
+}, 2000);
