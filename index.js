c76a784879e3bda8b60cf2b9934811cbb9cbd8ae
Sanjid Talukder <sanjidtalukder02@gmail.com>
2024-11-02 19:08:14 +0600

    Search bottom

--- index.js
+++ index.js
@@ -1,50 +1,16 @@
-// initializing some values
-let totalAttempts = 5;
-let attempts = 0;
-let totalWons = 0;
-let totallosts = 0;
 
-// finding elements
-const form = document.querySelector("form");
-const cardBody = document.querySelector(".card-body");
-const guessingNumber = form.querySelector("#guessingNumber");
-const checkButton = form.querySelector("#check");
-const resultText = cardBody.querySelector(".resultText");
-const lostWonMessage = document.createElement("p");
-const remainingAttempts = cardBody.querySelector(".remainingAttempts");
+let select=document.getElementById("select");
+let list=document.getElementById("list");
+let selectText=document.getElementById("selectText");
+let inputfield=document.getElementById("inputfield");
+let options=document.getElementsByClassName("options");
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  attempts++;
-  if (attempts === 5) {
-    guessingNumber.disabled = true;
-    checkButton.disabled = true;
-  }
-  if (attempts < 6) {
-    let guessNumber = Number(guessingNumber.value);
-    checkResult(guessNumber);
-    remainingAttempts.innerHTML = `Remaining attempts: ${
-      totalAttempts - attempts
-    }`;
-  }
-  guessingNumber.value = "";
-});
-
-function checkResult(guessingNumber) {
-  const randomNumber = getRandomNumber(5);
-  if (guessingNumber === randomNumber) {
-    resultText.innerHTML = `you have won`;
-    totalWons++;
-  } else {
-    resultText.innerHTML = `you have lost; random number was: ${randomNumber}`;
-    totallosts++;
-  }
-  lostWonMessage.innerHTML = `Won: ${totalWons}, Lost: ${totallosts}`;
-  lostWonMessage.classList.add("large-text");
-  cardBody.appendChild(lostWonMessage);
+select.onclick=function(){
+    list.classList.toggle("open");
 }
-
-function getRandomNumber(limit) {
-  return Math.floor(Math.random() * limit) + 1;
+for(option of options){
+option.onclick=function(){
+    selectText.innerHTML=this.innerHTML;
+    inputfield.placeholder= "search In "+  selectText.innerHTML;
+}
 }
